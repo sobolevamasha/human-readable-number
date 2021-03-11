255d742ed4053dc1b53f3f23bd7c1f303db120fc
Masha <77979222+sobolevamasha@users.noreply.github.com>
2021-03-11 13:44:14 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
+    const units = [
+      "zero",
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+      "ten",
+      "eleven",
+      "twelve",
+      "thirteen",
+      "fourteen",
+      "fifteen",
+      "sixteen",
+      "seventeen",
+      "eighteen",
+      "nineteen",
+    ];
+    const dozens = [
+      "",
+      "",
+      "twenty",
+      "thirty",
+      "forty",
+      "fifty",
+      "sixty",
+      "seventy",
+      "eighty",
+      "ninety",
+    ];
   
-}
+    const numberToString = (number) => {
+      if (number < 20) return units[number];
+      let digit = number % 10;
+      if (number < 100)
+        return dozens[Math.floor(number / 10)] + (digit ? " " + units[digit] : "");
+      if (number < 1000)
+        return (
+          units[Math.floor(number / 100)] +
+          " hundred" +
+          (number % 100 == 0 ? "" : " " + numberToString(number % 100))
+        );
+      return (
+        numberToString(Math.floor(number / 1000)) +
+        " thousand" +
+        (number % 1000 != 0 ? "" + numberToString(number % 1000) : " ")
+      );
+    };
+  
+    return numberToString(number);
+  };
